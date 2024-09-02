50ce4fd0ea8f5004ec0ab93fbbf31834996a8bbc
trankien <kien8b2003@gmail.com>
2024-09-03 00:25:13 +0700

    update

--- src/main/java/vn/hoidanit/jobhunter/service/UserService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/UserService.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.domain.dto.CreateUserDTO;
 import vn.hoidanit.jobhunter.domain.dto.FetchUserDTO;
-import vn.hoidanit.jobhunter.domain.dto.Meta;
 import vn.hoidanit.jobhunter.domain.dto.ResultPaginationDTO;
 import vn.hoidanit.jobhunter.domain.dto.UpdateUserDTO;
 import vn.hoidanit.jobhunter.repository.UserRepository;
@@ -45,7 +44,7 @@ public class UserService {
   public ResultPaginationDTO getAllUsers(Specification<User> spec, Pageable pageable) {
     Page<User> pageUser = this.userRepository.findAll(spec, pageable);
     ResultPaginationDTO rs = new ResultPaginationDTO();
-    Meta mt = new Meta();
+    ResultPaginationDTO.Meta mt = new ResultPaginationDTO.Meta();
     List<FetchUserDTO> listFetchUserDTOs = new ArrayList<FetchUserDTO>();
 
     mt.setPage(pageable.getPageNumber() + 1);
